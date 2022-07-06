e552e383a8ceb74b8f56664156bedc689ab5e2bc
cocodyq <55238881+cocodyq@users.noreply.github.com>
2022-07-07 00:43:02 +0800

    ENH initial setup

--- setup.py
+++ setup.py
@@ -9,20 +9,20 @@ setup(
     url="https://github.com/cocodyq/GMSC-Mapper-demo",  
     author="Yiqian Duan",
     author_email="yqduan20@fudan.edu.cn",  
-	license="MIT"
+    license="MIT"
     classifiers=[  
         "Development Status :: 3 - Alpha",
         "Topic :: Scientific/Engineering :: Bio-Informatics",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3.8",
     ],
-    package_dir={"GMSC-Mapper-demo": "GMSC-mapper"},  # Optional
-    packages=[GMSC-mapper,GMSC-mapper.data],  # Requiredes
+    package_dir={"GMSC-Mapper-demo": "GMSC-mapper"}, 
+    packages=[GMSC-mapper,GMSC-mapper.data],  
 	include_package_data=True,
     python_requires=">=3.8",
     install_requires=open("./requirements.txt", "r").read().splitlines(),
     package_data={"GMSC-Mapper-demo": ["GMSC-mapper.data"]},
-	zip_safe=False,
+    zip_safe=False,
     entry_points={  
         "console_scripts": [
             "GMSC-mapper=GMSC-mapper.cli:main",
