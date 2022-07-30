fd70c9273361cd9aec4490d5f4343acda61f4261
cocodyq <1057696203@qq.com>
2022-07-30 14:24:58 +0800

    ENH add summary & fix setup

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
+from os import path
 from setuptools import setup, find_packages
 
 setup(
     name="GMSC-mapper",  
     version="0.0.1_alpha", 
-    description="Annotate smORFs based on the Global Microbial smORFs Catalog (GMSC)",  
+    description="Command line tool to query the Global Microbial smORFs Catalog (GMSC)",  
     long_description=open("./README.md", "r").read(),  
     long_description_content_type="text/markdown",  
-    url="https://github.com/cocodyq/GMSC-Mapper-demo",  
+    url="https://github.com/BigDataBiology/GMSC-mapper",  
     author="Yiqian Duan",
     author_email="yqduan20@fudan.edu.cn",  
     license="MIT"
@@ -16,16 +17,13 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3.8",
     ],
-    package_dir={"GMSC-Mapper-demo": "GMSC-mapper"}, 
-    packages=[GMSC-mapper,GMSC-mapper.data],  
+    package_dir={"GMSC-mapper": "GMSC-mapper"}, 
+    packages=['GMSC-mapper'],  
     include_package_data=True,
-    python_requires=">=3.8",
-    install_requires=open("./requirements.txt", "r").read().splitlines(),
-    package_data={"GMSC-Mapper-demo": ["GMSC-mapper.data"]},
+    python_requires='>=3.8',
+    install_requires=open('./requirements.txt', 'r').read().splitlines(),
     zip_safe=False,
     entry_points={  
-        "console_scripts": [
-            "GMSC-mapper=GMSC-mapper.cli:main",
-        ],
-    },
+        "console_scripts": ['GMSC-mapper=GMSC-mapper.main:main'],
+    }
 )
